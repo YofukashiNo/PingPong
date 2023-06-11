28fa07e404e0d35e11c20bc60ae0e1c09e0b4623
Tharki-god <alokender018@gmail.com>
2023-06-11 13:34:45 +0530

    v0.9.2

--- src/lib/requiredModules.ts
+++ src/lib/requiredModules.ts
@@ -1,9 +1,7 @@
 import { webpack } from "replugged";
 import * as Types from "../types";
-export const GuildReadStateStore = webpack.getByProps(
+export const GuildReadStateStore = webpack.getByProps<Types.GuildReadStateStore>(
   "getMentionCount",
   "getTotalMentionCount",
-) as unknown as Types.GuildReadStateStore;
-export const ReadStateStore = webpack.getByProps(
-  "getReadStatesByChannel",
-) as unknown as Types.ReadStateStore;
+);
+export const ReadStateStore = webpack.getByProps<Types.ReadStateStore>("getReadStatesByChannel");
